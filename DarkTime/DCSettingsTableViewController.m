fd9bcae9acc42e444c03e8495598532dd43833da
Luke LaBonte <luke@magicalsoftware.org>
2011-10-21 21:52:46 -0500

    Put all of the initiation in one place.  If the Slider does not exist it will do what needs to be done to create it.

--- DarkTime/DCSettingsTableViewController.m
+++ DarkTime/DCSettingsTableViewController.m
@@ -222,14 +222,13 @@
 
 - (void)createBrightnessCell:(NSIndexPath *)indexPath cell:(UITableViewCell *)cell
 {
-    CGRect sliderRect = CGRectMake(170, 0, 280, 45);
-    
     if (!self.brightnessSlider) {
-        self.brightnessSlider = [[UISlider alloc] initWithFrame:sliderRect];
+        CGRect sliderRect = CGRectMake(170, 0, 280, 45);
+        
+        self.brightnessSlider               = [[UISlider alloc] initWithFrame:sliderRect];
+        self.brightnessSlider.minimumValue  = 0.0;
+        self.brightnessSlider.maximumValue  = 1.0;
     }
-    
-    self.brightnessSlider.minimumValue = 0.0;
-    self.brightnessSlider.maximumValue = 1.0;
     
     NSString *path = [[NSBundle mainBundle] 
                       pathForResource:@"brightness-dim" 
